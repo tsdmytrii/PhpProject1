41fc947bea28137b595211be6213ce4a4b73226a
Dima <dima@Dima.(none)>
2013-02-16 14:42:00 +0200

    CLASS DB ADDED

--- Public/post.php
+++ Public/post.php
@@ -1,3 +1,15 @@
+<html>
+    <head>
+        <script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/jquery.lightbox-0.5.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/jquery.lightbox-0.5.css" media="screen" />
+<script type="text/javascript">
+$(function() {
+	$('a').lightBox(); 
+	});
+</script>
+    </head>
+    <body>
 <?php
 //$link1 = mysql_connect("localhost", "root", "dimadima");
 //mysql_select_db("passwords", $link1);
@@ -18,18 +30,43 @@ if ($row = mysql_fetch_array($result)) {
         echo "<br>";
         $name = "$id" . "_200px";
         $name1 = "$id" . "_100px";
+        
+        //echo "<img src='http://localhost/PhpProject1/uploads/$name1.jpeg'>";  
+        echo "<a href='http://localhost/PhpProject1/uploads/$id.jpeg'> <img src='http://localhost/PhpProject1/uploads/$name1.jpeg' width='72' height='72' alt='' /></a>";                                        
+  
         ?>
-        <a href="" class="b-socials__link"><img src="http://localhost/PhpProject1/uploads/<?php echo "$name1"; ?>.jpeg " 
-                                                onmouseover="this.src='http://localhost/PhpProject1/uploads/<?php echo "$name"; ?>.jpeg'"
-                                                onmouseout="this.src='http://localhost/PhpProject1/uploads/<?php echo "$name1"; ?>.jpeg'"
-                                                alt="Fuck"></a>;        
 
+<script>
+        (function(){
+
+$(function(){
+
+var cropped_postfix = '100px', resized_postfix = '200px';
+
+$('img').hover(function () {
+
+$(this).attr('src', $(this).attr('src').replace(cropped_postfix, resized_postfix));
+
+},
+
+function () {
+
+$(this).attr('src', $(this).attr('src').replace(resized_postfix, cropped_postfix));
+
+}
+
+);
+
+});
+
+})();
+</script>
         <?php
-        //echo "<img class=forhover src='http://localhost/PhpProject1/uploads/$name.jpeg' alt='Fuck'";
-
-        echo "</td></tr>";
+              echo "</td></tr>";
     } while ($row = mysql_fetch_array($result));
 }
 echo "</table>";
 printf("<a href=\"posts.php?page=1\"> Back </a>");
 ?>
+    </body>
+</html>
